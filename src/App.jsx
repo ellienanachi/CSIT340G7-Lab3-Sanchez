8a53c90c5cf3fd912058c5836537c889eb144bd7
LAB24_USER ON WS43 <L24X17W43@LabHCI.net>
2026-09-18 17:58:00 +0800

    1.1: course information step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <hr />
+      <p>{props.fullName} - {props.courseCode} - {props.section}</p>
+    </footer>
+  )
+}
+
 const App = () => {
-  const course = 'Industry Elective 1'
-  const part1 = 'Introduction to React'
-  const exercises1 = 10
-  const part2 = 'Anatomy of React'
-  const exercises2 = 7
-  const part3 = 'React Hooks'
-  const exercises3 = 14
+  const course = 'CSIT340 - Applications Development and Emerging Technologies'
+  const part1 = 'CSIT321 (Database Systems)'
+  const exercises1 = 3
+  const part2 = 'MATH211 (Discrete Mathematics)'
+  const exercises2 = 3
+  const part3 = 'CSIT227 (Object-Oriented Programming)'
+  const exercises3 = 3
+
+  const student = {
+    fullName: 'Chad Ellie Sanchez',
+    courseCode: 'CSIT340',
+    section: 'ITG7'
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+      <Footer
+        fullName={student.fullName}
+        courseCode={student.courseCode}
+        section={student.section}
+      />
     </div>
   )
 }
